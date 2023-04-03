33f3dd2d64627a9a4f0d8573b8492d850155e846
Kauanzzyn <kauan_a_rocha@estudante.sesisenai.org.br>
2023-04-03 07:54:59 -0300

    new_prog

---
+++ app.js
@@ -0,0 +1,23 @@
+const frase = "estudando programação"  // contante =>rótulo
+
+const PI = 3.1415927
+
+console.log(frase)
+console.log(PI)
+
+
+let texto = "computação" // variável => não aceita sobre escrito
+console.log(texto)
+console.log('-------------------------')
+texto  = "computador"
+console.log(texto)
+console.log('-------------------------')
+var texto2 = "teste de variavel"   //variável => acita sobrescrito
+console.log(texto2)
+console.log('-------------------------')
+var texto2 = "nova frase"
+console.log(texto2)
+console.log('-------------------------')
+
+texto3 ="teste de variável"   // declaração feita pelo entepretados do  JS
+console.log(texto3)
